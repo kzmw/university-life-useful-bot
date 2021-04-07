0a27b8310106fb0ddb15decfab615f62891eecce
Kazuma Nishimura <kazumanombiri@icloud.com>
2021-04-07 22:44:02 +0900

    test

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from datetime import datetime
 from pytz import timezone
 import urllib.request
 from urllib.parse import urlencode
-import random, string
+import random
+import string
 import pprint
 import psycopg2
 
@@ -47,36 +48,26 @@ DB_TABLE = "line_data"
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+
 @app.route("/login")
 def login():
-        statenumber = ''.join([random.choice(string.ascii_letters + string.digits) for i in range(10)])
+        statenumber = ''.join(
+            [random.choice(string.ascii_letters + string.digits) for i in range(10)])
         return redirect("https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1655825338&redirect_uri=https://university-life-useful-bot.herokuapp.com/post&state=" + statenumber + "&scope=profile%20openid&disable_ios_auto_login=true")
+
 
 @app.route("/post")
 def post():
         code = request.args.get('code', '')
         state = request.args.get('state', '')
-        response = requests.post('https://api.line.me/oauth2/v2.1/token', data={'grant_type':'authorization_code','code':code,'redirect_uri':'https://university-life-useful-bot.herokuapp.com/post','client_id':'1655825338','client_secret':'23252a4f6b865a32a80427f71e89fe53'})
+        response = requests.post('https://api.line.me/oauth2/v2.1/token', data={'grant_type': 'authorization_code', 'code': code,
+                                 'redirect_uri': 'https://university-life-useful-bot.herokuapp.com/post', 'client_id': '1655825338', 'client_secret': '23252a4f6b865a32a80427f71e89fe53'})
         if response.status_code == 200:
             response_json = response.json()
             id_token = response_json["id_token"]
-            response2 = requests.post('https://api.line.me/oauth2/v2.1/verify', data={'id_token':id_token,'client_id':'1655825338'})
+            response2 = requests.post('https://api.line.me/oauth2/v2.1/verify', data={'id_token': id_token, 'client_id': '1655825338'})
             if response2.status_code == 200:
                 response2_json = response2.json()
-                conn = psycopg2.connect("postgresql://"
-                        + DB_USERNAME
-                        + ":" 
-                        + DB_PASSWORD
-                        + "@"
-                        + DB_HOSTNAME
-                        + ":"
-                        + "5432"
-                        + "/" 
-                        + DB_NAME)
-                cur = conn.cursor()
-                sql_sentence = "select count(*) from " + DB_TABLE + ";"
-                cur.execute(sql_sentence)
-                rows = json.dumps(cur.fetchall())
                 cur.close()
                 conn.close()
                 session["flag"] = True
@@ -89,10 +80,11 @@ def post():
         else:
             return 'エラー'
 
+
 @app.route("/edit")
 def edit():
     if "flag" in session and session["flag"]:
-        return session["name"] + "さん、編集ページへようこそ！"
+        return render_template("name.html")
     else:
         return "かえれ！！！！！！"
 
