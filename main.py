cd3d7b9da0ed06f7d6b804f7c541cf1216bd05ba
Kazuma Nishimura <kazumanombiri@icloud.com>
2021-04-21 23:14:58 +0900

    renew:main.py new_user

--- main.py
+++ main.py
@@ -135,12 +135,32 @@ def submit():
                 return session["name"] + "の設定を保存しました。"
                 break
         else:
-            sql = "INSERT INTO " + DB_TABLE + " VALUES ('" + request.form["university_name"] + "','" + request.form["university_department"] + "','" + request.form["home_station"] + "','" + request.form["university_station"] + "','" + request.form["train_line1"] + "','" + request.form["train_line2"] + "','" + request.form["train_line3"] + "','" + request.form["train_line4"]
+            sql = "INSERT INTO " + DB_TABLE + " VALUES ('" + request.form["university_name"] + "','" + request.form["university_department"] + "','" + request.form["home_station"] + "','" + \
+                request.form["university_station"] + "','" + request.form["train_line1"] + "','" + \
+                request.form["train_line2"] + "','" + \
+                request.form["train_line3"] + "','" + \
+                request.form["train_line4"]
             for i in range(1, 36, 1):
-                sql += "','" + request.form["tt" + str(i) + "_name"] + "','" + request.form["tt" + str(i) + "_teacher"] + "','" + request.form["tt" + str(i) + "_room"] + ")"
+                sql += "','" + request.form["tt" + str(i) + "_name"] + "','" + request.form["tt" + str(
+                    i) + "_teacher"] + "','" + request.form["tt" + str(i) + "_room"] + ")"
             cur.execute(sql)
             con.commit()
             return session["name"] + "の設定を保存しました。"
+
+
+@app.route("/admin")
+def admin():
+    if session["uid"] == "U787b3a631ebfdaf0cadd81e045597805":
+        con = psycopg2.connect("host=" + DB_HOSTNAME +
+                               " port=" + "5432" +
+                               " dbname=" + DB_NAME +
+                               " user=" + DB_USERNAME +
+                               " password=" + DB_PASSWORD)
+        sql = 'select * from ' + DB_TABLE
+        with con.cursor() as cur:
+            cur.execute(sql)
+            rows = cur.fetchall()
+            render_template("admin.html", user_data=rows)
 
 
 @app.route("/callback", methods=['POST'])
